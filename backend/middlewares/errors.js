160191ff243369f0a2bf34d1740a9edf5b2a8023
FatmaMama <fatma_mama@hotmail.fr>
2021-10-01 22:51:50 +0100

    mongoose ObjectId and validation error

--- backend/middlewares/errors.js
+++ backend/middlewares/errors.js
@@ -1,3 +1,4 @@
+const ErrorHandler = require("../utils/errorHandler");
 
 const errorMiddleware = (err, req, res, next) => {
     err.statusCode = err.statusCode || 500;
@@ -5,15 +6,31 @@ const errorMiddleware = (err, req, res, next) => {
     if(process.env.NODE_ENV === 'DEVELOPMENT'){
         res.status(err.statusCode).json({
             success: false,
+            error: err,
             errMessage : err.message,
-            // stack: err.stack
+            stack: err.stack
         })
     };
     
     if(process.env.NODE_ENV === "PRODUCTION"){
+        let error = { ...err};
+        error.message = err.message;
+
+        //Wrong mongoose Object Id error
+        if(err.name === "CastError"){
+            const message = `Resource not found. Invalid ${err.path}`;
+            error = new ErrorHandler(message, 400)
+        }
+
+        //Handling Mongoose Validation error
+        if(err.name === "ValidatorError"){
+            const message = Object.values(err.errors).map(value => value.message);
+            error = new ErrorHandler(message, 400)
+        }
+
         res.status(err.statusCode).json({
             success: false,
-            message: err.message || "Internal Server Error"
+            message: error.message || "Internal Server Error"
         })
     }
 }
