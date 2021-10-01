df68ec18040e8471fc8dbaa4b5089f98f1aa4f2b
FatmaMama <fatma_mama@hotmail.fr>
2021-10-01 22:09:37 +0100

    handle uncaught exceptions

--- backend/server.js
+++ backend/server.js
@@ -6,6 +6,13 @@ const errorMiddleware = require('./middlewares/errors');
 
 app.use(express.json())
 
+//Handle Uncaught Exceptions
+process.on('uncaughtException', err => {
+    console.log(`ERROR: ${err.message}`);
+    console.log('Shutting down due to Uncaught Exception');
+    process.exit(1)
+});
+
 //set up config file
 dotenv.config({ path: 'backend/config/config.env' });
 
@@ -25,6 +32,6 @@ const server = app.listen(process.env.PORT, ()=> {
 //Handle Unhandled Promise Rejections
 process.on('unhandledRejection', (err) => {
     console.log(`ERROR: ${err.message}`);
-    console.log('Shutting down the server due to Unhandled Promise Rejection');
+    console.log('Shutting down the server due to Unhandled Promise Rejections');
     server.close(()=> process.exit(1))
 })
