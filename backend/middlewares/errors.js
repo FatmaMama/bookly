772d508e718c5092ab1ddbcd9bae9e6a90e0a098
FatmaMama <fatma_mama@hotmail.fr>
2021-10-06 17:11:42 +0100

    handle wrong and expired jwt token errors in prod mode

--- backend/middlewares/errors.js
+++ backend/middlewares/errors.js
@@ -20,13 +20,31 @@ const errorMiddleware = (err, req, res, next) => {
         if(err.name === "CastError"){
             const message = `Resource not found. Invalid ${err.path}`;
             error = new ErrorHandler(message, 400)
-        }
+        };
 
         //Handling Mongoose Validation error
         if(err.name === "ValidatorError"){
             const message = Object.values(err.errors).map(value => value.message);
             error = new ErrorHandler(message, 400)
-        }
+        };
+
+        //Handling Mongoose duplicate key error
+        if(err.code === 11000){
+            const message = `Duplicate ${Object.keys(err.keyValue)} entered`;
+            error = new ErrorHandler(message, 400)
+        };
+
+        //Handling wrong jwt token
+        if(err.name === 'jsonWebTokenError'){
+            const message = `Json Web Token is Invalid`;
+            error = new ErrorHandler(message, 400)
+        };
+
+        //Handling expired jwt token
+        if(err.name === 'TokenExpiredError'){
+            const message = `Json Web Token is Expired`;
+            error = new ErrorHandler(message, 400)
+        };
 
         res.status(err.statusCode).json({
             success: false,
