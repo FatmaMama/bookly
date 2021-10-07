825af6642522308840c5caf476750346c9eadcf6
FatmaMama <fatma_mama@hotmail.fr>
2021-10-07 12:24:51 +0100

    update/process order

--- backend/controllers/orderControllers.js
+++ backend/controllers/orderControllers.js
@@ -52,7 +52,7 @@ exports.myOrders = catchAsyncErrors ( async (req, res, next) => {
     })
 });
 
-//Get all orders   api/v1/admin/orders
+//Get all orders - ADMIN   api/v1/admin/orders
 exports.allOrders = catchAsyncErrors ( async (req, res, next) => {
     const orders = await Order.find();
 
@@ -67,3 +67,33 @@ exports.allOrders = catchAsyncErrors ( async (req, res, next) => {
         orders
     })
 });
+
+//update/Process order - ADMIN   api/v1/admin/order/:id
+exports.updateOrder = catchAsyncErrors ( async (req, res, next) => {
+    const order = await Order.findById(req.params.id);
+
+    if(order.orderStatus === 'Delivered'){
+        return next(new ErrorHandler('this order is already delivered', 400))
+    };
+
+    order.orderItems.forEach(async item => {
+        await updateStock(item.product, item.quantity)
+    });
+
+    order.orderStatus = req.body.status;
+    order.deliveredAt = Date.now();
+    await order.save();
+
+    res.status(200).json({
+        success : true,
+        order
+    })
+});
+
+async function updateStock (id, quantity){
+    const product = await Product.findById(id);
+
+    product.stock = product.stock - quantity;
+
+    await product.save( { validateBeforeSave : false });
+}
--- backend/routes/orderRoutes.js
+++ backend/routes/orderRoutes.js
@@ -1,7 +1,7 @@
 const express = require('express');
 const router = express.Router();
 const { isAuthenticated, authorizeRoles } = require('../middlewares/auth');
-const { newOrder, myOrders, getSingleOrder, allOrders } = require('../controllers/orderControllers');
+const { newOrder, myOrders, getSingleOrder, allOrders, updateOrder } = require('../controllers/orderControllers');
 
 router.post('/order/new', isAuthenticated, newOrder);
 
@@ -10,5 +10,7 @@ router.get('/order/:id', isAuthenticated, getSingleOrder);
 router.get('/orders/me', isAuthenticated, myOrders);
 
 router.get('/admin/orders', isAuthenticated, authorizeRoles('admin'), allOrders);
+
+router.put('/admin/order/:id', isAuthenticated, authorizeRoles('admin'), updateOrder);
 
 module.exports = router;
