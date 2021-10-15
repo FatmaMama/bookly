fd6f07992364efabc978ccba69b22c2be05068ce
FatmaMama <fatma_mama@hotmail.fr>
2021-10-15 15:52:17 +0100

    header footer homePage

---
+++ frontend/src/components/layouts/Footer.js
@@ -0,0 +1,13 @@
+import React, { Fragment } from 'react'
+
+export default function Footer() {
+    return (
+        <Fragment>
+            <footer className="py-1">
+      <p className="text-center mt-1">
+        Fantasia - 2021 -  All Rights Reserved
+      </p>
+    </footer>
+        </Fragment>
+    )
+}
