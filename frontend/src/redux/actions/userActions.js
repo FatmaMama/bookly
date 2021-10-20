e835180b6fa675e9de448d1acf36fa2c6e72ab04
FatmaMama <fatma_mama@hotmail.fr>
2021-10-20 11:29:59 +0100

    new password reducer and action

--- frontend/src/redux/actions/userActions.js
+++ frontend/src/redux/actions/userActions.js
@@ -19,6 +19,9 @@ import { LOGIN_REQUEST,
     FORGOT_PASSWORD_REQUEST,
     FORGOT_PASSWORD_SUCCESS,
     FORGOT_PASSWORD_FAIL,
+    NEW_PASSWORD_REQUEST,
+    NEW_PASSWORD_SUCCESS,
+    NEW_PASSWORD_FAIL,
     CLEAR_ERRORS
 } from "../constants/userConstants";
 
@@ -183,6 +186,31 @@ export const forgotPassword = (email) => async (dispatch) => {
     } catch (error) {
         dispatch({
             type : FORGOT_PASSWORD_FAIL,
+            payload: error.response.data.message
+        })
+    }
+};
+
+export const newPassword = (token, passwords) => async (dispatch) => {
+    try {
+        dispatch({ type: NEW_PASSWORD_REQUEST })
+ 
+        const config = {
+            headers: {
+                "content-type" : "application/json"
+            }
+        }
+ 
+        const { data } = await axios.post(`/api/v1/password/reset/${token}`, passwords, config);
+ 
+        dispatch({
+            type: NEW_PASSWORD_SUCCESS,
+            payload: data.success
+        })
+        
+    } catch (error) {
+        dispatch({
+            type : NEW_PASSWORD_FAIL,
             payload: error.response.data.message
         })
     }
